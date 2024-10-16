d6412e49b70a79436c8f47bb090a3cfd4d75bb84
Ariel Sebastián Sánchez Añazco <121512401+arielssa@users.noreply.github.com>
2024-10-15 22:04:04 -0500

    Removing Temps

--- src/main/java/com.agricultura/controller/ListarAgricultoresServlet.java
+++ src/main/java/com.agricultura/controller/ListarAgricultoresServlet.java
@@ -1,13 +1,11 @@
 package com.agricultura.controller;
 
 import com.agricultura.dao.AgricultorDAO;
-import com.agricultura.model.Agricultor;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.*;
 import java.io.IOException;
-import java.util.List;
 
 @WebServlet("/listarAgricultores")
 public class ListarAgricultoresServlet extends HttpServlet {
@@ -21,12 +19,9 @@ public class ListarAgricultoresServlet extends HttpServlet {
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        // Obtener la lista de agricultores
-        // Implementa un método en AgricultorDAO para listar todos
-        List<Agricultor> agricultores = agricultorDAO.listarTodosAgricultores();
-        request.setAttribute("agricultores", agricultores);
 
-        // Forward a la JSP de listado
+        request.setAttribute("agricultores", agricultorDAO.listarTodosAgricultores());
         request.getRequestDispatcher("/jsp/agricultores/listar.jsp").forward(request, response);
+
     }
 }
